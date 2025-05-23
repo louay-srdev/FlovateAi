f6c682d79ea213e73dae70db091f238ac4cfeac5
unknown <haddoulamezlini@gmail.com>
2025-05-24 00:03:08 +0100

    fixed errors

--- data/partners.js
+++ data/partners.js
@@ -60,6 +60,58 @@ export const partners = [
     className: "item-5",
   },
 ];
+export const partners2 = [
+  {
+    id: 1,
+    src: "/assets/images/partners/Spotify svg white.png",
+    alt: "Spotify",
+  },
+  {
+    id: 2,
+    src: "/assets/images/partners/Dropbox svg white.png",
+    alt: "Dropbox",
+  },
+  {
+    id: 3,
+    src: "/assets/images/partners/Google svg white.png",
+    alt: "Google",
+  },
+  {
+    id: 4,
+    src: "/assets/images/partners/Airbnb svg white.png",
+    alt: "Airbnb",
+  },
+  {
+    id: 5,
+    src: "/assets/images/partners/Envato svg white.png",
+    alt: "Envato",
+  },
+  {
+    id: 6,
+    src: "/assets/images/partners/Spotify svg white.png",
+    alt: "Spotify",
+  },
+  {
+    id: 7,
+    src: "/assets/images/partners/Dropbox svg white.png",
+    alt: "Dropbox",
+  },
+  {
+    id: 8,
+    src: "/assets/images/partners/Google svg white.png",
+    alt: "Google",
+  },
+  {
+    id: 9,
+    src: "/assets/images/partners/Airbnb svg white.png",
+    alt: "Airbnb",
+  },
+  {
+    id: 10,
+    src: "/assets/images/partners/Envato svg white.png",
+    alt: "Envato",
+  },
+];
 export const partnersImages = [
   {
     id: 1,
--- data/services.js
+++ data/services.js
@@ -30,3 +30,47 @@ export const services = [
     text: "Analyze Media Generator",
   },
 ];
+export const servicesItems = [
+  {
+    id: 1,
+    iconClass: "icon-copy-writing-1",
+    title: "Writing Blog Content",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+  {
+    id: 2,
+    iconClass: "icon-wishlist-1",
+    title: "eCommerce Copy",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+  {
+    id: 3,
+    iconClass: "icon-internet-1",
+    title: "Website Copy",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+  {
+    id: 4,
+    iconClass: "icon-megaphone-1",
+    title: "Marketing Copy",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+  {
+    id: 5,
+    iconClass: "icon-network-1",
+    title: "Social Media Content",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+  {
+    id: 6,
+    iconClass: "icon-network-2",
+    title: "Digital Ad Copy",
+    description: "We denounce righteous indigna <br /> and dislike men",
+    link: "#",
+  },
+];
